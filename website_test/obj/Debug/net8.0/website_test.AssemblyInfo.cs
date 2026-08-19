58ba53eaf925de092995f553f2c3d1168a449d84
zaina tariq <zainaalhusainy1@gmail.com>
2026-08-19 20:30:04 +0300

    zaina_new

--- website_test/obj/Debug/net8.0/website_test.AssemblyInfo.cs
+++ website_test/obj/Debug/net8.0/website_test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("website_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e363660cac354c495501ce3bf07b538aa0f6f77")]
 [assembly: System.Reflection.AssemblyProductAttribute("website_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("website_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
